33dc04ca8bcab7adb17e9349517229c7c5b11345
acevancho <aevancho@me.com>
2025-07-01 12:06:25 -0500

    move to src, controllers, and routes

---
+++ server/src/controllers/authController.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from 'express';
+import Agent from '../models/Agent';
+
+export const login = async (req: Request, res: Response) => {
+  console.log('Login request received:', req.body);
+  const { email } = req.body;
+
+if (!email) {
+  res.status(400).json({ error: 'Email is required' });
+  return;
+}
+  try {
+    const agent = await Agent.findOne({ email });
+    if (!agent) {
+      res.status(404).json({ error: 'Agent not found' });
+      return;
+    }
+
+    const token = `AGENT::${agent._id}`;
+    res.json({ token, agent: { _id: agent._id, email: agent.email } });
+    return;
+  } catch (err) {
+    res.status(500).json({ error: 'Login failed' });
+    return;
+  }
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,15 +2,21 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectDB } from './db';
+// import propertyRoutes from './properties';
+import authRoutes from './routes/auth';
+
+// router.use('/properties', propertyRoutes);
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 connectDB();
 
 app.use(cors());
 app.use(express.json());
+app.use('/auth', authRoutes);
+
 
 app.get('/', (req, res) => {
   res.send('Server is up!');
