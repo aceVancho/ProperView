4217627721b222e9165e809c8ae810ffa540ad4e
acevancho <aevancho@me.com>
2025-07-01 15:53:38 -0500

    Agent.ts comments

---
+++ server/src/routes/agent.ts
@@ -0,0 +1,10 @@
+// Why is this file empty?
+
+// GET /properties/agent is more resource-focused
+// The agent is just a sub-scope
+
+// GET /agent/properties puts "agent" as the resource
+// Since we're not user-specific scoping...
+// GET /users/:userId/posts
+// GET /customers/:id/orders
+// I opted to stick with the resource-focused approach
