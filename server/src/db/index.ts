74df0621bfdcc79ca6f52d936d0a47d06e222450
Ace <aevancho@me.com>
2025-07-03 10:04:16 -0500

    Hard code connection string

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -2,10 +2,12 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 dotenv.config();
 
+// Obviously would be in .env file in production
+const MONGODB_CONNECTION_STRING="mongodb+srv://admin:p%40ssw0rd@megpt-cluster.ldjp43p.mongodb.net/ProperViewDB?retryWrites=true&w=majority&appName=ProperViewDB"
+
 export const connectDB = async () => {
   try {
-    console.log('Connecting to MongoDB...', process.env.MONGODB_CONNECTION_STRING);
-    const connection = await  mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+    const connection = await  mongoose.connect(MONGODB_CONNECTION_STRING);
     console.log(`MongoDB connected: ${connection.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error}`);
